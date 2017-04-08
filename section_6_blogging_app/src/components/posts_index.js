a9c79a81e711bcf783b87b7309e0eeb0913d1d2c
Lucas Bärenfänger <mail@scalarookie.com>
2017-04-08 20:09:41 +0200

    Add code of section 6, lecture 81

--- section_6_blogging_app/src/components/posts_index.js
+++ section_6_blogging_app/src/components/posts_index.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/index';
 
 class PostsIndex extends Component {
   componentWillMount() {
-    console.log('this would be a good time to call an action creator to fetch posts');
+    this.props.fetchPosts();
   }
   render() {
     return (
       <div>List of blog posts</div>
-    )
+    );
   }
 }
 
-export default PostsIndex;
+export default connect(null, { fetchPosts })(PostsIndex);
