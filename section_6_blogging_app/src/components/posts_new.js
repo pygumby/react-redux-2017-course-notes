bcd55badd3b86741537442771c5aaeeafb4b6f21
Lucas Bärenfänger <mail@scalarookie.com>
2017-04-15 01:26:07 +0200

    Add code of section 6, lecture 87

--- section_6_blogging_app/src/components/posts_new.js
+++ section_6_blogging_app/src/components/posts_new.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
 
 class PostsNew extends Component {
   render() {
     const { fields: { title, categories, content }, handleSubmit } = this.props;
     return (
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(this.props.createPost)}>
         <h3>Create A New Post</h3>
         <div className="form-group">
           <label>Title</label>
@@ -25,7 +26,10 @@ class PostsNew extends Component {
   }
 }
 
+// connect: 1st argument is mapStateToProps, 2nd is mapDispatchToProps
+// reduxForm: 1st is form config, 2nd is mapStateToProps, 3rd is mapDispatchToProps
+
 export default reduxForm({
   form: 'PostsNewForm',
   fields: ['title', 'categories', 'content']
-})(PostsNew);
+}, null, { createPost })(PostsNew);
